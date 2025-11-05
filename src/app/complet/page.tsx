64ca55274f8b83edbf6d4db14e51ee59373c8c7f
pxttxrxwxn <naysasatadur5555@gmail.com>
2025-11-05 10:29:59 +0700

    57

--- src/app/complet/page.tsx
+++ src/app/complet/page.tsx
@@ -197,7 +197,7 @@ export default function Complet() {
                   </linearGradient>
                 </defs>
               </svg>
-              <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-[63%] pt-5 z-40 flex flex-col items-center gap-3">
+              <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-[63%] pt-5 z-40 flex flex-col items-center gap-3 w-full">
                 <h1 className="text-[18px] text-[#4557c7] font-bold font-[prompt]">
                   อัปเดตจำนวนกระทงล่าสุด
                 </h1>
